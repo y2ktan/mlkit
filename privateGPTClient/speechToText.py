5e83d214af1e8440701e99931345acf3a9dd6bb9
Tan Chun Mun <chunmuntan@yahoo.com>
2024-03-30 01:10:40 +0800

    added vox

--- privateGPTClient/speechToText.py
+++ privateGPTClient/speechToText.py
@@ -1,10 +1,30 @@
+import os
+
 import speech_recognition as sr
+import io
+import wave
+import pyaudioconvert as pac
+import json
+import uuid
+from vosk import Model, KaldiRecognizer
+
+MODEL_PATH = "vosk-model-small-en-us-0.15"
 
 
 def speech_to_text(audio_source) -> str:
-    r = sr.Recognizer()
-    with sr.AudioFile(audio_source) as source:
-        audio_text = r.listen(source)
-    result = r.recognize_google(audio_text)
-    print("result: "+result)
-    return result
+    recognizer = KaldiRecognizer(Model(MODEL_PATH), 16000)
+
+    source_audio_file = str(uuid.uuid4()) + ".wav"
+    target_audio_file = str(uuid.uuid4()) + ".wav"
+    audio_source.save(source_audio_file)
+
+    pac.convert_wav_to_16bit_mono(source_audio_file, target_audio_file)
+    with wave.open(target_audio_file) as wav_file:
+        audio_data = wav_file.readframes(wav_file.getnframes())
+    recognizer.AcceptWaveform(audio_data)
+    result = recognizer.Result()
+    res = json.loads(result)
+    print(res["text"])
+    os.remove(source_audio_file)
+    os.remove(target_audio_file)
+    return res["text"]
